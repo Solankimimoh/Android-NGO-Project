c716ae77510562f56c226afdfca0dde5eeb33d65
solankimimoh <solankimimoh@gmail.com>
2019-01-31 23:10:52 +0530

    first init

---
+++ UserNgo/app/src/main/java/com/example/ngouser/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.ngouser;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler;
+    private Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                final Intent gotoLoginScreen = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(gotoLoginScreen);
+                finish();
+            }
+        };
+        handler.postDelayed(runnable, 3000);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(runnable);
+    }
+}
