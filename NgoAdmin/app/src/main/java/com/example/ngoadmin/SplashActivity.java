a776a9a827ddeb50a64b7cc4e620ab2766e6d6f9
solankimimoh <solankimimoh@gmail.com>
2019-01-31 23:12:55 +0530

    first init

---
+++ NgoAdmin/app/src/main/java/com/example/ngoadmin/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.example.ngoadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
